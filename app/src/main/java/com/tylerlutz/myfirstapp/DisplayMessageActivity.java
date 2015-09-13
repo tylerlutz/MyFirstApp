dc8a3a95aece51c60e9642d89ba63b1b7765d8a0
Tyler Lutz <tlutz.computers@gmail.com>
2015-09-13 16:39:57 -0400

    Added function to the DisplayMessageActivity
    Now when the user Enters text into the textbox and clicks send, the new activity will display their message.

--- app/src/main/java/com/tylerlutz/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/tylerlutz/myfirstapp/DisplayMessageActivity.java
@@ -4,13 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
     }
 
     @Override
