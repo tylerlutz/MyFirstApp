38fecfe2693af31291e5640e317ea25afee8d776
Tyler Lutz <tlutz.computers@gmail.com>
2015-09-13 16:23:45 -0400

    Added files to commit

--- app/src/main/java/com/tylerlutz/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/tylerlutz/myfirstapp/DisplayMessageActivity.java
@@ -4,23 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.content.Intent;
-import android.widget.EditText;
 
-public class MyActivity extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "com.tylerlutz.myfirstapp.MESSAGE";
+public class DisplayMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.activity_display_message);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_my, menu);
+        getMenuInflater().inflate(R.menu.menu_display_message, menu);
         return true;
     }
 
@@ -38,14 +34,4 @@ public class MyActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-
 }
